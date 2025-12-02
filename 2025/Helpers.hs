bfd4515ee80135e233319162a23fe1ab2ee286c6
Peter van de Werken <peter@van-de-werken.nl>
2025-12-02 21:18:34 +0100

    solved day02, both parts by brute force :/

--- 2025/Helpers.hs
+++ 2025/Helpers.hs
@@ -1,5 +1,29 @@
 module Helpers where
 
+import Data.List
+
 solve :: Show a => String -> (String -> a) -> IO ()
 solve nr solver = let filename = "input-day" ++ nr ++ ".txt"
                    in readFile filename >>= putStrLn . show . solver
+
+split :: Eq a => a -> [a] -> ([a], [a])
+split x = mapSnd (drop 1) . span (/= x)
+
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn _ [] = []
+splitOn x xs = first : splitOn x rest
+  where
+    (first, rest) = split x xs
+
+mapFst :: (a -> b) -> (a, c) -> (b, c)
+mapSnd :: (a -> b) -> (c, a) -> (c, b)
+mapFst f (x, y) = (f x, y)
+mapSnd f (x, y) = (x, f y)
+
+chuncksOf :: Int -> [a] -> [[a]]
+chuncksOf _ [] = []
+chuncksOf i xs = uncurry (:) . mapSnd (chuncksOf i) $ splitAt i xs
+
+mapAdjacent :: (a -> a -> b) -> [a] -> [b]
+mapAdjacent _ (_:[]) = []
+mapAdjacent f (a:b:cs) = f a b : mapAdjacent f (b:cs)
